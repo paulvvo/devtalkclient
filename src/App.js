2aa154d948a3b88d277e757810175ba234486890
paulvvo <paulvo@live.ca>
2018-12-03 03:23:13 -0500

    added components layout, finished navbar component and have the basic unfinished template for the landing and footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//Components
+import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-		
+				<Navbar/>
 				<h1>Hello World</h1>
+
       </div>
     );
   }
---
+++ src/components/layout/Landing.js
@@ -0,0 +1,16 @@
+// import React, {Component} from 'react';
+import React from "react";
+
+const Landing = () =>{
+	return(
+		<div>Hi boi</div>
+	)
+}
+
+// function Landing(){
+// 	return(
+// 		<div>hi</div>
+// 	)
+// }
+
+export default Landing;
