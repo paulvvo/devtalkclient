927781978b018d53537578d969431baf7d737d8e
paulvvo <paulvo@live.ca>
2018-12-04 03:12:04 -0500

    installed react-router-dom  for routing different components, added routr for Landing component, created empty template components for Login and Register

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
+
+
 
 //Components
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 
-
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-				<Navbar/>
-				<Landing/>
-				<Footer/>
-      </div>
+    return  (
+			<Router>
+				<div className="App">
+					<Navbar/>
+					<Route exact path="/" component={ Landing }/>
+					<Footer/>
+				</div>
+			</Router>
     );
   }
 }
