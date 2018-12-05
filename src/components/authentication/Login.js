3df315ab22d3c55ec2891cf9a3d9469033280846
paulvvo <paulvo@live.ca>
2018-12-04 23:52:49 -0500

    added component state for the login component and added onchange method for email and password input and updates the state, form now calls onsubmit method and returns logged user

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,6 +1,28 @@
 import React, {Component} from "react";
 
 class Login extends Component{
+	constructor(){
+		super();
+		this.state = {
+			email:"",
+			password:"",
+			errors:{},
+		}
+	}
+
+	onChange = (event) =>{
+		this.setState({[event.target.name]:event.target.value});
+	}
+	onSubmit = (event) =>{
+		event.preventDefault();
+		const loggedUser = {
+			email:this.state.email,
+			password:this.state.password,
+		}
+
+		console.log(loggedUser);
+	}
+
 	render(){
 		return(
 			<div className="login">
@@ -9,12 +31,12 @@ class Login extends Component{
 						<div className="col-md-8 m-auto">
 							<h1 className="display-4 text-center">Log In</h1>
 							<p className="lead text-center">Sign in to your DevConnector account</p>
-							<form action="dashboard.html">
+							<form onSubmit={this.onSubmit}>
 								<div className="form-group">
-									<input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" />
+									<input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange}/>
 								</div>
 								<div className="form-group">
-									<input type="password" className="form-control form-control-lg" placeholder="Password" name="password" />
+									<input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange}/>
 								</div>
 								<input type="submit" className="btn btn-info btn-block mt-4" />
 							</form>
