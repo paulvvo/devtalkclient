9d2921054c8d0bf21b881c3f45793b32fac3c573
paulvvo <paulvo@live.ca>
2018-12-11 22:28:52 -0500

    app will now check if there is a token in local storage from login so that it will put it in the header everytime you refresh page, this will keep the state for the current user even after refresh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import {Provider} from "react-redux";
+import jwtDecode from "jwt-decode"
+
 import store from "./store";
 import './App.css';
-
-
+import setAuthToken from "./utils/setAuthToken";
+import {SET_CURRENT_USER} from './actions/types';
 
 
 //Components
@@ -14,6 +16,16 @@ import Footer from "./components/layout/Footer";
 import Register from "./components/authentication/Register";
 import Login from "./components/authentication/Login";
 
+if(localStorage.jwt){
+	setAuthToken(localStorage.jwt);
+
+	const decoded = jwtDecode(localStorage.jwt);
+
+	store.dispatch({
+		type:SET_CURRENT_USER,
+		payload: decoded
+	})
+}
 
 
 class App extends Component {
