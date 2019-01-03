31cf359a4f63d43ceddeede22d31effcc1ea44ba
paulvvo <paulvo@live.ca>
2019-01-02 21:25:11 -0500

    added mapStateToProps and proptypes to createProfile component and added initial state for createprofile component

--- src/components/createProfile/CreateProfile.js
+++ src/components/createProfile/CreateProfile.js
@@ -4,6 +4,26 @@ import PropTypes from "prop-types";
 
 
 class CreateProfile extends Component{
+	constructor(){
+		super();
+		this.state = {
+			displaySocialInputs:false,
+			handle: '',
+			company: '',
+			website: '',
+			location: '',
+			status: '',
+			skills: '',
+			githubusername: '',
+			bio: '',
+			twitter: '',
+			facebook: '',
+			linkedin: '',
+			youtube: '',
+			instagram: '',
+			errors: {},
+		}
+	}
 	render(){
 		return(
 			<div></div>
@@ -11,4 +31,14 @@ class CreateProfile extends Component{
 	}
 }
 
+CreateProfile.propTypes = {
+	profile:PropTypes.object.isRequired,
+	errors: PropTypes.object.isRequired,
+}
+const mapStateToProps = (state) => {
+	return({
+		profiles:state.profileReducer,
+		errors:state.errorReducer,
+	})
+}
 export default connect(null)(CreateProfile);
