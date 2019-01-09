13c3fe6c80d853fe7f7a1b437f220371d5a7e1f8
paulvvo <paulvo@live.ca>
2019-01-08 21:21:17 -0500

    created and added component inputgroup, and imported different components into createProfile components

---
+++ src/components/common/InputGroup.js
@@ -0,0 +1,48 @@
+import React from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+
+const InputGroup  = ({
+	name,
+	placeholder,
+	value,
+	error,
+	icon,
+	type,
+	onChange,
+}) =>{
+	return(
+		<div className="input-group mb-3">
+			{error? <div className="red">{error}</div>:null}
+			<div className="input-group-prepend">
+				<span className="input-group-text">
+					<i className={icon}/>
+				</span>
+			</div>
+			<input
+			className={classnames('form-control form-control-lg', {
+				'is-invalid':error
+			})}
+			placeholder={placeholder}
+			name={name}
+			value={value}
+			onChange={onChange}/>
+		</div>
+	)
+}
+
+
+InputGroup.propTypes = {
+	name:PropTypes.string.isRequired,
+	placeholder:PropTypes.string,
+	value:PropTypes.string.isRequired,
+	error:PropTypes.string,
+	icon:PropTypes.string,
+	type:PropTypes.string.isRequired,
+	onChange:PropTypes.func.isRequired,
+}
+InputGroup.defaults = {
+	type:'text'
+}
+
+export default InputGroup;
--- src/components/createProfile/CreateProfile.js
+++ src/components/createProfile/CreateProfile.js
@@ -4,6 +4,9 @@ import PropTypes from "prop-types";
 
 //Components
 import TextFieldGroup from "../common/TextFieldGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import InputGroup from "../common/InputGroup";
+import SelectListGroup from "../common/SelectListGroup";
 
 class CreateProfile extends Component{
 	constructor(){
