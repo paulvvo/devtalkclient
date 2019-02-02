71a00b4343b7602c89c582157e363717101833b3
paulvvo <paulvo@live.ca>
2019-02-01 23:47:35 -0500

    created empty component templates for addexperience component

---
+++ src/components/addCredentials/AddExperience.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class AddExperience extends Component{
+	render(){
+		returns(
+			<div>Add Exp Component</div>
+		)
+	}
+}
+
+export default AddExperience;
