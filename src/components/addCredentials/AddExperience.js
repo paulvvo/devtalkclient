fb751016fb930f277a080e61e252f79e4b4d61ea
paulvvo <paulvo@live.ca>
2019-02-02 23:55:52 -0500

    imported components and packages into addexperience component, and added initial state in constructor

--- src/components/addCredentials/AddExperience.js
+++ src/components/addCredentials/AddExperience.js
@@ -1,11 +1,39 @@
 import React, {Component} from "react";
+import {Link, withRouter} from "react-router-dom";
+import {TextFieldGroup} from "../common/TextFieldGroup";
+import {TextFieldGroup} from "../common/TextAreaFieldGroup";
+import {connect} from "react-redux";
+import PropTypes from "prop-types";
+
+
 
 class AddExperience extends Component{
+	constructor(props){
+		super(props);
+		this.state={
+			company:"",
+			title:"",
+			location:"",
+			from: "",
+			to: "",
+			current: false,
+			description: "",
+			errors: {},
+			disabled:false,
+		}
+	}
 	render(){
 		returns(
 			<div>Add Exp Component</div>
 		)
 	}
 }
+const mapStateToProps = (state) =>{
+	return({
+		profiles: state.profileReducer,
+		errors: state.errorsReducer
+	})
+}
 
-export default AddExperience;
+
+export default connect(mapStateToProps, {})(withRouter(AddExperience));
