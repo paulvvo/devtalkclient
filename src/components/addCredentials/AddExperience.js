4ded9d0be860106a4b100b101bb8d10bf3068e4a
paulvvo <paulvo@live.ca>
2019-02-04 21:32:27 -0500

    added more input fields (textfieldgroup component) for add experience component

--- src/components/addCredentials/AddExperience.js
+++ src/components/addCredentials/AddExperience.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import {Link, withRouter} from "react-router-dom";
-import {TextFieldGroup} from "../common/TextFieldGroup";
-import {TextAreaFieldGroup} from "../common/TextAreaFieldGroup";
+import TextFieldGroup from "../common/TextFieldGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
 
@@ -30,9 +30,40 @@ class AddExperience extends Component{
 					<div className="row">
 						<div className="col-md-8 m-auto">
 							<Link to="/dashboard" className="btn btn-light">Go Back</Link>
-							<h1 className="display-4 text-center"></h1>
+							<h1 className="display-4 text-center">Add Experience</h1>
 							<p className="lead text-center">Add Current Job or Any Past Experience</p>
 							<small className="d-block pb-3">* = required fields</small>
+							<form onSubmit={this.onSubmit}>
+								<TextFieldGroup
+									placeholder="* Company"
+									name="company"
+									value={this.state.company}
+									onChange={this.onChange}
+									error={errors.company}
+								/>
+								<TextFieldGroup
+									placeholder="* Job Title"
+									name="title"
+									value={this.state.title}
+									onChange={this.onChange}
+									error={errors.title}
+								/>
+								<TextFieldGroup
+									placeholder="Location"
+									name="location"
+									value={this.state.location}
+									onchange={this.onChange}
+									error={errors.location}
+								/>
+								<h6>From Date</h6>
+								<TextFieldGroup
+									name="from"
+									type="date"
+									value={this.state.from}
+									onChange={this.onChange}
+									error={errors.from}
+								/>
+							</form>
 						</div>
 					</div>
 				</div>
