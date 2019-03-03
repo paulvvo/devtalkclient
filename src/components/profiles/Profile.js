e1c0985ac08569be2458ce639e56749880624c66
paulvvo <paulvo@live.ca>
2019-03-02 23:52:29 -0500

    created empty template profile component for displaying list of developers

---
+++ src/components/profiles/Profile.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react"
+import {connect} from "react-redux";
+import PropTypes from "prop-types";
+
+class Profile extends Component{
+	render(){
+		return(<div>hi</div>)
+	}
+}
+
+export default Profile;
