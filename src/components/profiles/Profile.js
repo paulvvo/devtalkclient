628f3388d7eb8db15ced5212e362ed6a32380ec0
paulvvo <paulvo@live.ca>
2019-03-03 23:28:55 -0500

    imported getprofiles action(not created yet) and other components, connect profiles component to store and added proptypes

--- src/components/profiles/Profile.js
+++ src/components/profiles/Profile.js
@@ -2,10 +2,23 @@ import React, {Component} from "react"
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
 
-class Profile extends Component{
+//Components
+import Loading from "../common/Loading";
+
+//Actions
+import getProfiles from "../../actions/profileActions";
+
+class Profiles extends Component{
 	render(){
-		return(<div>hi</div>)
+		return(<div>hi< /div>)
 	}
 }
+Profiles.propTypes = {
+	profile: PropTypes.object.isRequired,
+	getProfiles: PropTypes.func.isRequired,
+};
 
-export default Profile;
+const mapStateToProps = state => ({
+	profile: state.profileReducer
+});
+export default connect(mapStateToProps, {})(Profiles);
