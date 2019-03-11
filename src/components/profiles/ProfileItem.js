c2154a954c836d7a096d5096fd7d5c79c756fd7d
paulvvo <paulvo@live.ca>
2019-03-10 21:57:54 -0400

    added profile item information into component, profile picture, name, job, location, etc

--- src/components/profiles/ProfileItem.js
+++ src/components/profiles/ProfileItem.js
@@ -1,8 +1,26 @@
 import React, {Component} from "react";
+import {Link} from "react-router-dom";
+
+import isEmpty from "../../validation/isEmpty";
 
 class ProfileItem extends Component{
 	render(){
-		return(<div>hi</div>)
+		const {profile} = this.props;
+		return(
+			<div className="card card-body bg-light mb-3">
+				<div className="row">
+					<div className="col-2">
+						<img src="profile.user.avatar" alt="" classname="rounded-circle"/>
+					</div>
+					<div className="col-lg-6 col-md-4 col-8">
+						<h3>{profile.user.name}</h3>
+						<p>{profile.status} {isEmpty(profile.company)? null: profile.company}</p>
+						<p>{isEmpty(profile.location)? null : profile.location}</p>
+						<Link to={`/profile/${profile.handle}`} className="btn btn-info">view profile</Link>
+					</div>
+				</div>
+			</div>
+		)
 
 	}
 }
