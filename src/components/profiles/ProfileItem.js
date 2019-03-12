c25e7880860fa18aaf54c07cdc8aca970c6668c1
paulvvo <paulvo@live.ca>
2019-03-11 21:10:48 -0400

    added map through list of skills for profileItems component

--- src/components/profiles/ProfileItem.js
+++ src/components/profiles/ProfileItem.js
@@ -18,11 +18,27 @@ class ProfileItem extends Component{
 						<p>{isEmpty(profile.location)? null : profile.location}</p>
 						<Link to={`/profile/${profile.handle}`} className="btn btn-info">view profile</Link>
 					</div>
+					<div className="col-md-4 d-none d-md-block">
+						<h4>Skill Set</h4>
+						<ul className="list-group">
+							{
+								profile.skills.slice(0,4).map((skill, i) =>{
+									<li key={i} className="list-group-item">
+										<i className="fa fa-check pr-1"/>
+										{skill}
+									</li>
+
+								})
+							}
+						</ul>
+					</div>
 				</div>
 			</div>
 		)
 
 	}
 }
-
+ProfileItem.propTypes={
+	profiles: PropTypes.Object.isRequired
+}
 export default ProfileItem;
