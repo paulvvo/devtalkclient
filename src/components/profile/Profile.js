cc100de9c2ce1420a49dfe1276140b1e45cac09c
paulvvo <paulvo@live.ca>
2019-03-26 21:40:28 -0400

    pass profile state as props into ProfileAbout component and filled out profile information(name, bio, and skills) in ProfileAbout component

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -36,7 +36,7 @@ class Profile extends Component{
 
 					</div>
 					<ProfileHeader profile={profile}/>
-					<ProfileAbout/>
+					<ProfileAbout profile={profile}/>
 					<ProfileCreds/>
 					<ProfileGithub/>
 				</div>
--- src/components/profile/ProfileAbout.js
+++ src/components/profile/ProfileAbout.js
@@ -1,30 +1,31 @@
 import React, {Component} from "react";
 
 class ProfileAbout extends Component{
+
 	render(){
+		const {profile} = this.props;
+		console.log(profile);
 		return(
 			<div className="row">
 				<div className="col-md-12">
 					<div className="card card-body bg-light mb-3">
-						<h3 className="text-center text-info">John's Bio</h3>
-						<p className="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident fuga cum necessitatibus blanditiis vel,
-							officia facere porro esse numquam assumenda doloremque saepe aliquam nemo excepturi aliquid maiores! Excepturi,
-							libero repudiandae.
+						<h3 className="text-center text-info">{profile.user.name}'s Bio</h3>
+						<p className="lead">{profile.bio}
 						</p>
 						<hr />
 						<h3 className="text-center text-info">Skill Set</h3>
 						<div className="row">
 							<div className="d-flex flex-wrap justify-content-center align-items-center">
-								<div className="p-3">
-									<i className="fa fa-check"></i> HTML</div>
-								<div className="p-3">
-									<i className="fa fa-check"></i> CSS</div>
-								<div className="p-3">
-									<i className="fa fa-check"></i> JavaScript</div>
-								<div className="p-3">
-									<i className="fa fa-check"></i> Python</div>
-								<div className="p-3">
-									<i className="fa fa-check"></i> C#</div>
+
+							{
+								profile.skills.map((item, index)=>{
+									return(
+										<div key={index} className="p-3">
+											<i className="fa fa-check"></i> {item}</div>
+									)
+								})
+							}
+
 							</div>
 						</div>
 					</div>
