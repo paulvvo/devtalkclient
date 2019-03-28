3fc91a68311400afdc168e968c61e4a3e7965d0a
paulvvo <paulvo@live.ca>
2019-03-27 22:43:39 -0400

    passed education and experience as props into profilecreds component and fixed issue with empty bio in profileAbout component

--- src/components/profile/ProfileAbout.js
+++ src/components/profile/ProfileAbout.js
@@ -1,16 +1,22 @@
 import React, {Component} from "react";
+import PropTypes from "prop-types";
+import isEmpty from "../../validation/isEmpty";
+
 
 class ProfileAbout extends Component{
 
 	render(){
 		const {profile} = this.props;
-		console.log(profile);
+		// console.log(profile);
 		return(
 			<div className="row">
 				<div className="col-md-12">
 					<div className="card card-body bg-light mb-3">
 						<h3 className="text-center text-info">{profile.user.name}'s Bio</h3>
-						<p className="lead">{profile.bio}
+						<p className="lead">
+						{
+							isEmpty(profile.bio)? null : (<span>{profile.bio}</span>)
+						}
 						</p>
 						<hr />
 						<h3 className="text-center text-info">Skill Set</h3>
--- src/components/profile/ProfileCreds.js
+++ src/components/profile/ProfileCreds.js
@@ -2,6 +2,9 @@ import React, {Component} from "react";
 
 class ProfileCreds extends Component{
 	render(){
+		const {experience, education} = this.props;
+		console.log(experience);
+		console.log(education);
 		return(
 			<div>ProfileCreds</div>
 		)
