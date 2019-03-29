beefd65c1e8988dc53523ed15511e0065c1e1685
paulvvo <paulvo@live.ca>
2019-03-28 22:22:09 -0400

    fill out ProfileCreds component with profile experience props

--- src/components/profile/ProfileCreds.js
+++ src/components/profile/ProfileCreds.js
@@ -1,12 +1,32 @@
 import React, {Component} from "react";
+import Moment from "react-moment";
 
 class ProfileCreds extends Component{
 	render(){
 		const {experience, education} = this.props;
-		console.log(experience);
-		console.log(education);
+		// console.log(experience);
+		// console.log(education);
+		const expItems = experience.map(expItem =>{
+			return(
+				<li key={expItem._id} className="list-group-item">
+					<h4>{expItem.company}</h4>
+					<p>
+						<Moment format="YYYY/MM/DD">{expItem.from}</Moment> - {expItem.to===null ? "Now" : <span><Moment format="YYYY/MM/DD">{expItem.to}</Moment></span>}
+					</p>
+					<p>
+						<strong>Position: </strong>
+						{expItem.title}
+					</p>
+					<p>
+						{expItem.description === '' ? null : <span><strong>Description: </strong>{expItem.description}</span>}
+					</p>
+				</li>
+			)
+		})
 		return(
-			<div>ProfileCreds</div>
+			<div>
+				{expItems}
+			</div>
 		)
 	}
 }
