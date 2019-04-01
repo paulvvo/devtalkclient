c9e81d1a458c1c845aa36592173971a7b36b992f
paulvvo <paulvo@live.ca>
2019-03-31 21:55:31 -0400

    hotfix empty social media error in ProfileHeader component and passed props to ProfileGithub component

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -38,7 +38,10 @@ class Profile extends Component{
 					<ProfileHeader profile={profile}/>
 					<ProfileAbout profile={profile}/>
 					<ProfileCreds education={profile.education} experience={profile.experience}/>
-					<ProfileGithub/>
+					{
+						profile.githubusername ? <ProfileGithub/> : <div>shit</div>
+					}
+
 				</div>
 			)
 		}
