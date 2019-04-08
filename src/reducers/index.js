b5319c6849a4aa0891911bf314d595b0718d7b17
paulvvo <paulvo@live.ca>
2019-04-07 22:11:04 -0400

    created postActions, added new constants for types, include postReducer in combineReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import profileReducer from "./profileReducer";
+import postReducer from "./postReducer";
 
 export default combineReducers({
 	authReducer: authReducer,
 	errorReducer: errorReducer,
 	profileReducer: profileReducer,
+	postReducer: postReducer,
 })
