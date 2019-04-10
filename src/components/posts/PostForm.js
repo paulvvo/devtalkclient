e8a1bbd365f3d3f7235cfdf7e178cc674a872395
paulvvo <paulvo@live.ca>
2019-04-09 21:23:14 -0400

    created empty state in constructor for PostForm component, created input forms with textarea and a button

--- src/components/posts/PostForm.js
+++ src/components/posts/PostForm.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
 
-const PostForm = () =>{
-	return(
-		<div>Post Form</div>
-	)
+//Components
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+//Action
+import {addPost} from "../../actions/postActions";
+
+class PostForm extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			text:"",
+			errors:{}
+		}
+	}
+	render(){
+		return(
+			<div className="post-form mb-3">
+				<div className="card card-info">
+					<div className="card-header bg-info text-white">Post a comment..</div>
+					<div className="card-body">
+						<form action="">
+							<div className="form-group">
+								<textarea className="form-control form-control-lg" placeholder="Create a Post"/>
+							</div>
+							<button className="btn btn-dark">Submit</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default PostForm;
