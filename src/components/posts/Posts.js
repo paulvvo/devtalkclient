413a1504b64d4966ae6dd907d20e90b8b6f0c894
paulvvo <paulvo@live.ca>
2019-04-14 17:38:52 -0400

    connect Posts component to store, componentDidMount calls action get posts, check for posts or loading in render method

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,12 +1,32 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
+import PropTypes from "prop-types";
 
 //Components
 import PostForm from "./PostForm";
 import Loading from "../common/Loading";
 
+//Actions
+import {getPost} from "../../actions/postActions";
+
+
 class Posts extends Component{
+	componentDidMount(){
+		this.props.getPost();
+	}
 	render(){
+		const {posts, loading} = this.props.posts;
+		let postContent;
+		if(posts === null || posts.length <1 || loading){
+			postContent = (<Loading/>);
+		}else{
+			postContent = posts;
+			console.log(postContent);
+
+		}
+
+
+
 		return(
 			<div className="feed">
 				<div className="container">
@@ -21,5 +41,11 @@ class Posts extends Component{
 	}
 }
 
-
-export default Posts;
+Posts.propTypes = {
+	posts: PropTypes.object.isRequired,
+	getPost: PropTypes.func.isRequired,
+}
+const mapStateToProps = (state) => ({
+	posts: state.postReducer,
+})
+export default connect(mapStateToProps, {getPost})(Posts);
