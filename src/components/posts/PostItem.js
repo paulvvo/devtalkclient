e9d1de6f2c7aa1b41491b1be040ec5069eea61ca
paulvvo <paulvo@live.ca>
2019-04-18 23:03:25 -0400

    temp placeholder markup added postItem component

--- src/components/posts/PostItem.js
+++ src/components/posts/PostItem.js
@@ -9,7 +9,38 @@ class PostItem extends Component{
 		// console.log(post.text);
 		console.log(auth);
 		return(
-			<div>{post.text}</div>
+			<div class="posts">
+
+				<div class="card card-body mb-3">
+					<div class="row">
+						<div class="col-md-2">
+							<a href="profile.html">
+								<img class="rounded-circle d-none d-md-block" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
+									alt="" />
+							</a>
+							<br />
+							<p class="text-center">John Doe</p>
+						</div>
+						<div class="col-md-10">
+							<p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint possimus corporis sunt necessitatibus! Minus
+								nesciunt soluta suscipit nobis. Amet accusamus distinctio cupiditate blanditiis dolor? Illo perferendis
+								eveniet cum cupiditate aliquam?</p>
+							<button type="button" class="btn btn-light mr-1">
+								<i class="text-info fas fa-thumbs-up"></i>
+								<span class="badge badge-light">4</span>
+							</button>
+							<button type="button" class="btn btn-light mr-1">
+								<i class="text-secondary fas fa-thumbs-down"></i>
+							</button>
+							<a href="post.html" class="btn btn-info mr-1">
+								Comments
+							</a>
+
+						</div>
+					</div>
+				</div>
+
+			</div>
 		)
 	}
 }
