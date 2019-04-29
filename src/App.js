398e6e28564609996957f68c50e192812c7dacae
paulvvo <paulvo@live.ca>
2019-04-28 20:08:25 -0400

    created post component empty template, created privateroute in app for Post Component

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import NotFound from "./components/notFound/NotFound";
 import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 
 if(localStorage.jwt){
 	setAuthToken(localStorage.jwt);
@@ -84,6 +85,10 @@ class App extends Component {
 							</Switch>
 							<Switch>
 								<PrivateRoute exact path="/feed" component={Posts}/>
+							</Switch>
+
+							<Switch>
+								<PrivateRoute exact path="/post/:id" component={Post}/>
 							</Switch>
 							<Route exact path="/not-found" component={NotFound}/>
 						</div>
---
+++ src/components/post/Post.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class Post extends Component{
+	render(){
+		return(
+			<div>Component: Post</div>
+		)
+	}
+}
+
+export default Post;
