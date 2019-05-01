c786dc7a65f9bbcbc466f6f1a5582e7994115377
paulvvo <paulvo@live.ca>
2019-04-30 21:38:45 -0400

    created get_post case for post reducer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import {ADD_POST, POST_LOADING, GET_POSTS, DELETE_POST} from "../actions/types";
+import {ADD_POST, POST_LOADING, GET_POSTS, GET_POST, DELETE_POST} from "../actions/types";
 
 const initialState = {
 	post:{},
@@ -24,7 +24,12 @@ export default function (state=initialState, action) {
 				...state,
 				posts: [action.payload, ...state.posts]
 			}
-
+		case GET_POST:
+			return{
+				...state,
+				post: action.payload,
+				loading:false,
+			}
 		case DELETE_POST:
 			// return {
 			// 	...state,
