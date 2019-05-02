9fbd0af2ccc20eabfd1f78b0da8f27ce467df02e
paulvvo <paulvo@live.ca>
2019-05-01 21:54:43 -0400

    connect Post component to redux, import getPost action to post component

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,11 +1,23 @@
 import React, {Component} from "react";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+
+import {getPost} from "../../actions/postActions";
 
 class Post extends Component{
 	render(){
+		console.log(this.props.post);
 		return(
 			<div>Component: Post</div>
 		)
 	}
 }
 
-export default Post;
+Post.propTypes ={
+	post: PropTypes.object.isRequired,
+	getPost: PropTypes.func.isRequired
+}
+const mapStateToProps = state => ({
+	post: state.postReducer
+})
+export default connect(mapStateToProps, {getPost})(Post);
