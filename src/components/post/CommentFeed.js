8bc97eab1b0724bf9b2ce2cf45695bd72d8d817e
paulvvo <paulvo@live.ca>
2019-05-29 22:37:10 -0400

    created empty commentfeed component

---
+++ src/components/post/CommentFeed.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+
+class CommentFeed extends Component{
+	render(){
+		const {comments, postId} = this.props;
+		<div></div>
+	}
+}
+
+export default CommentFeed;
