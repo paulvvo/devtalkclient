b8d2cf83215275efb48ebdd0449f5303585a48e0
paulvvo <paulvo@live.ca>
2019-05-31 23:48:59 -0400

    added proptypes and connect to redux for commentItem component

--- src/components/post/CommentItem.js
+++ src/components/post/CommentItem.js
@@ -1,9 +1,25 @@
 import React, {Component} from "react";
+import {connect} from "redux";
+import PropTypes from "prop-types";
+
+//Actions
+import {deleteComment} from "../../actions/postActions";
 
 class CommentItem extends Component{
 	render(){
+		
 		return(<div>CommentItem Component</div>)
 	}
 }
 
-export default CommentItem;
+CommentItem.propTypes = {
+	deleteComment: PropTypes.func.isRequired,
+	comment : PropTypes.object.isRequired,
+	postId : PropTypes.string.isRequired,
+	auth: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = (state) =>({
+	auth: state.authReducer,
+})
+export default connect(mapStateToProps)(CommentItem);
