acc9ab75c08f37c62bc110c625671c625ac64adf
paulvvo <paulvo@live.ca>
2019-06-01 23:51:49 -0400

    markup for commentItem, filled in data

--- src/components/post/CommentItem.js
+++ src/components/post/CommentItem.js
@@ -7,7 +7,21 @@ import {deleteComment} from "../../actions/postActions";
 
 class CommentItem extends Component{
 	render(){
-		
+		const {comment, postId, auth} = this.props;
+		<div className="card card-body mb-3">
+			<div className="row">
+				<div className="col-md-2">
+					<a href="/profile">
+						<img src={comment.avatar} alt="" className="rounded-circle d-none d-md-block"/>
+						<br/>
+						<p className="text-center">{comment.name}</p>
+					</a>
+				</div>
+				<div className="col-md-10">
+					<p className="lead">{comment.text}</p>
+				</div>
+			</div>
+		</div>
 		return(<div>CommentItem Component</div>)
 	}
 }
